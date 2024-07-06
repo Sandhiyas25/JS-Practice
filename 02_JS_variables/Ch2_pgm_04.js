42241223bf513e766b9cb75862fb558da9da3157
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-06 20:33:45 +0530

    update

--- 02_JS_variables/Ch2_pgm_04.js
+++ 02_JS_variables/Ch2_pgm_04.js
@@ -1,13 +1,15 @@
 // Variables vary
 
 var score;
-score = 100;
-console.log(score);
-
 score = 150;
 console.log(score);
 
+var score2;
+score2 = 200;
+console.log(score2);
 
+score2=175;
+console.log(score2);
 
 /* Further Adventures
  *
