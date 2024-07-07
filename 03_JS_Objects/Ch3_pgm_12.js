a7974c4fc7b9d10e974127ebdc5ca01b09ca24a1
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-07 19:44:46 +0530

    update

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -8,6 +8,17 @@ var post = {
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
 
+console.log(post.id);
+console.log(post.title);
+console.log(post.author);
+
+var post2 = {
+	id : 2,
+	title : "Harry Potter",
+	author : "J.K. Rowling",
+	created : 1997,
+	body : "It is a story of good versus evil, as Harry and his friends..." 
+};
 
 
 /* Further Adventures
