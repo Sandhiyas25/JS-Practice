65b003675f000f17e09dd80c97af2d760402ea98
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-07 19:27:30 +0530

    update

--- 03_JS_Objects/Ch3_pgm_08.js
+++ 03_JS_Objects/Ch3_pgm_08.js
@@ -8,8 +8,18 @@ book = {
 	published : 1937
 };
 
+book2 = {
+	title : "Harry Potter",
+	author : "J.K. Rowling",
+	published : 1997
+}
 console.log(book.title);
 console.log(book.author);
+console.log(book.published);
+
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
 
 
 
