36a41f8119766f8ca88f52613ff3e9d4e676e749
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-07 19:24:56 +0530

    update

--- 03_JS_Objects/Ch3_pgm_07.js
+++ 03_JS_Objects/Ch3_pgm_07.js
@@ -5,14 +5,17 @@ var book2;
 
 book1 = {
     title : "The Hobbit",
-    author : "J. R. R. Tolkien"
+    author : "J. R. R. Tolkien",
+    published : 1997
 };
 
 book2 = {
 	title : "Northern Lights",
-	author : "Philip Pullman"
+	author : "Philip Pullman",
+    published : 1990
 };
-
+console.log(book1);
+console.log(book2);
 
 
 /* Further Adventures
