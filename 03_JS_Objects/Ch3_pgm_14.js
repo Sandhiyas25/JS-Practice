9b98873bb682eb90d5b2d898f3fe9eb7dd6c5962
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-07 20:30:08 +0530

    update

--- 03_JS_Objects/Ch3_pgm_14.js
+++ 03_JS_Objects/Ch3_pgm_14.js
@@ -10,7 +10,11 @@ var questionAndAnswer = {
 	marksForQuestion: 2
 };
 
-
+console.log(questionAndAnswer.question);
+console.log("1. " + questionAndAnswer.answer1);
+console.log("2. " + questionAndAnswer.answer2);
+console.log("3. " + questionAndAnswer.answer3);
+console.log("4. " + questionAndAnswer.answer4);
 
 /* Further Adventures
  *
