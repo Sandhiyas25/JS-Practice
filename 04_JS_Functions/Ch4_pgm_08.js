ab0b52ed162a65261760f2d35fb2b0868cd4a796
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-09 19:54:35 +0530

    update

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -1,15 +1,27 @@
 // Displaying a menu
 
-var displayMenu;
-
-displayMenu = function () {
-	console.log("Please choose an option:");
-	console.log("(1) Print log");
-	console.log("(2) Upload file");
-	console.log("(9) Quit");
+var displayMenu = function () {
+    console.log("Please choose an option:");
+    console.log("(1) Print log");
+    console.log("(2) Upload file");
+    console.log("(3) Delete user");
+    console.log("(4) Send message");
+    console.log("(9) Quit");
 };
 
 displayMenu();
+
+var displayMyMenu = function () {
+    console.log("Custom Menu:");
+    console.log("(1) View profile");
+    console.log("(2) Edit settings");
+    console.log("(3) Help");
+    console.log("(4) Logout");
+    console.log("(5) Exit");
+};
+
+displayMyMenu();
+
 
 
 
