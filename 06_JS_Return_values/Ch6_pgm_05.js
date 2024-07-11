0aeb2c39546bacdbca355d122a9be4d8d4483641
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-11 14:38:45 +0530

    update

--- 06_JS_Return_values/Ch6_pgm_05.js
+++ 06_JS_Return_values/Ch6_pgm_05.js
@@ -2,11 +2,11 @@
 
 var totalCost;
 
-totalCost = function (callOutCharge, costPerHour, numberOfHours) {
-	return callOutCharge + costPerHour * numberOfHours;
+totalCost = function (callOutCharge, costPerHour, numberOfHours, discount) {
+	return callOutCharge + costPerHour * numberOfHours - discount;
 };
 
-console.log("$" + totalCost(30, 40, 3));
+console.log("$" + totalCost(30, 40, 12, 10));
 
 
 
