cda73f084b208170ba0f59faa91198e652c14c90
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-13 19:01:35 +0530

    update

--- 08_JS_Arrays/Ch8_pgm_11.js
+++ 08_JS_Arrays/Ch8_pgm_11.js
@@ -1,19 +1,37 @@
 // Finding the total shopping bill
 
-var getTotalBill = function (itemCosts, itemCounts) {
-	var total = 0;
-
-	itemCosts.forEach(function (cost, i) {
-		total += cost * itemCounts[i];
-	});
-
-	return total;
-};
-
-var costs = [ 1.99, 4.95, 2.50, 9.87 ];
-var numOfEach = [ 2, 1, 5, 2 ];
+var costs = [ 1.99, 4.95, 2.50, 9.87, 3.75 ]; 
+var numOfEach = [ 2, 1, 5, 2, 3 ]; 
 
 console.log("The total cost is $" + getTotalBill(costs, numOfEach));
+var getTotalBill = function(itemCosts, itemCounts) {
+    var total = 0;
+
+    itemCounts.forEach(function(count, i) {
+        total += itemCosts[i] * count;
+    });
+
+    return total;
+};
+
+var items = [
+    { cost: 1.99, numberBought: 2 },
+    { cost: 4.95, numberBought: 1 },
+    { cost: 2.50, numberBought: 5 },
+    { cost: 9.87, numberBought: 2 },
+    { cost: 3.75, numberBought: 3 }
+];
+var getTotalBill = function(items) {
+    var total = 0;
+
+    items.forEach(function(item) {
+        total += item.cost * item.numberBought;
+    });
+
+    return total;
+};
+
+console.log("The total cost is $" + getTotalBill(items));
 
 
 
