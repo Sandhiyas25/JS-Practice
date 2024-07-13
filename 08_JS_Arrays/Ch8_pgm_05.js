dcd881b8c8f10c517421202569a6f0af7cd22dea
sandhiya <sivakumarsandhiya25@gmail.com>
2024-07-13 18:44:11 +0530

    update

--- 08_JS_Arrays/Ch8_pgm_05.js
+++ 08_JS_Arrays/Ch8_pgm_05.js
@@ -1,31 +1,57 @@
 // Passing an array to a function
 
-var getVisitorReport = function (visitorArray, dayInWeek) {
-	var days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday"
+var getVisitorReport = function(visitorArray, dayInWeek) {
+  var days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
   ];
-	var index = dayInWeek - 1;
-	var visitorReport;
+  var index = dayInWeek - 1;
+  var visitorReport;
 
-	visitorReport = "There were ";
+  visitorReport = "There were ";
   visitorReport += visitorArray[index];
   visitorReport += " visitors ";
-	visitorReport += "on " + days[index];
+  visitorReport += "on " + days[index];
 
-	return visitorReport;
+  return visitorReport;
 };
 
-var visitors = [ 354, 132, 210, 221, 481 ];
+var visitors = [354, 132, 210, 221, 481, 312, 176]; // Adjusted for Saturday and Sunday
 
-var report = getVisitorReport(visitors, 2);
+var reportFriday = getVisitorReport(visitors, 5);
+var reportSaturday = getVisitorReport(visitors, 6);
+var reportSunday = getVisitorReport(visitors, 7);
 
-console.log(report);
+console.log(reportFriday);   
+console.log(reportSaturday); 
+console.log(reportSunday);  
+
+var getMonthlyVisitorReport = function(monthArray, weekNumber, dayInWeek) {
+  var week = monthArray[weekNumber - 1]; 
+
+  
+  return getVisitorReport(week, dayInWeek);
+};
 
 
+var month = [
+  [354, 132, 210, 221, 481, 312, 176], 
+  [400, 150, 190, 200, 450, 300, 150], 
+  [370, 140, 220, 215, 490, 310, 180], 
+  [380, 160, 200, 210, 470, 320, 170] 
+];
+
+
+var reportWeek1Wednesday = getMonthlyVisitorReport(month, 1, 3); // Week 1, Wednesday
+var reportWeek2Sunday = getMonthlyVisitorReport(month, 2, 7);   // Week 2, Sunday
+
+console.log(reportWeek1Wednesday); 
+console.log(reportWeek2Sunday);    
 
 /* Further Adventures
  *
